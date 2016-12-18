f41e5aba3de144c0c569f69cf084eb66b0db05a7
Meheret Egzerab <meheretgirma22@gmail.com>
2016-12-17 18:22:23 -0700

    deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: "process.env.MONGODB_URI || mongodb://localhost/job",
+    database: process.env.MONGODB_URI || "mongodb://localhost/job",
 
     secret: "big fox running in the wild"
 }
